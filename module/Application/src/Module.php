6e48bcc5c0a0ac0014883f137733d517cfc5c27b
cuonghoang131993 <cuonghoang131993@gmail.com>
2023-10-17 00:21:18 +0700

    update config

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -25,35 +25,35 @@ class Module
         $serviceManager = $e->getApplication()->getServiceManager();
         $session = $serviceManager->get(SessionManager::class);
         $session->start();
-        $container = new Session\Container('initialized');
+        // $container = new Session\Container('initialized');
 
-        //let’s check if our session is not already created (for the guest or user)
-        if (isset($container->init)) {
-            return;
-        }
+        // //let’s check if our session is not already created (for the guest or user)
+        // if (isset($container->init)) {
+        //     return;
+        // }
 
-        //new session creation
-        $request = $serviceManager->get('Request');
-        $session->regenerateId(true);
-        $container->init = 1;
-        $container->remoteAddr = $request->getServer()->get('REMOTE_ADDR');
-        $container->httpUserAgent = $request->getServer()->get('HTTP_USER_AGENT');
-        $config = $serviceManager->get('Config');
-        $sessionConfig = $config['session'];
-        $chain = $session->getValidatorChain();
+        // //new session creation
+        // $request = $serviceManager->get('Request');
+        // $session->regenerateId(true);
+        // $container->init = 1;
+        // $container->remoteAddr = $request->getServer()->get('REMOTE_ADDR');
+        // $container->httpUserAgent = $request->getServer()->get('HTTP_USER_AGENT');
+        // $config = $serviceManager->get('Config');
+        // $sessionConfig = $config['session'];
+        // $chain = $session->getValidatorChain();
 
-        foreach ($sessionConfig['validators'] as $validator) {
-            switch ($validator) {
-                case Validator\HttpUserAgent::class:
-                    $validator = new $validator($container->httpUserAgent);
-                    break;
-                case Validator\RemoteAddr::class:
-                    $validator = new $validator($container->remoteAddr);
-                    break;
-                default:
-                    $validator = new $validator();
-            }
-            $chain->attach('session.validate', array($validator, 'isValid'));
-        }
+        // foreach ($sessionConfig['validators'] as $validator) {
+        //     switch ($validator) {
+        //         case Validator\HttpUserAgent::class:
+        //             $validator = new $validator($container->httpUserAgent);
+        //             break;
+        //         case Validator\RemoteAddr::class:
+        //             $validator = new $validator($container->remoteAddr);
+        //             break;
+        //         default:
+        //             $validator = new $validator();
+        //     }
+        //     $chain->attach('session.validate', array($validator, 'isValid'));
+        // }
     }
 }
